af8d26f6313d9192cd311f470211df1caeead955
Jacobo <gilmith@gmail.com>
2025-01-01 19:35:34 +0100

    feat: tiradas

---
+++ src/app/models/personaje.ts
@@ -0,0 +1,10 @@
+export interface Personaje {
+    fuerza: number;
+    destreza : number;
+    carisma : number;
+    constitucion : number;
+    sabiduria : number;
+    inteligencia : number;
+    nombre : string;
+    
+}
