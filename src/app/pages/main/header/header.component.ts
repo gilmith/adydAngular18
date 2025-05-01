02c43b5a38b3555d5730e271e71e21ffc79e1125
jacobo <gilmith@gmail.com>
2025-05-01 23:01:49 +0200

    boton de logout

--- src/app/pages/main/header/header.component.ts
+++ src/app/pages/main/header/header.component.ts
@@ -1,18 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { OffcanvasService } from '../../../services/offcanvas/offcanvas.service';
+import {Router, RouterModule} from "@angular/router";
 
 @Component({
     selector: 'app-header',
-    imports: [],
+    imports: [RouterModule],
     templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+    styleUrl: './header.component.css',
+    standalone: true
 })
 export class HeaderComponent {
 
   public sidebar = inject(OffcanvasService);
+  public router : Router = inject(Router);
 
-  public toogle() {
+  public toggle() {
     this.sidebar.update();
   }
 
+  logout() {
+    this.router.navigateByUrl('/login').then(r => sessionStorage.removeItem('token'));
+  }
 }
