fc6292e3c9f5e8e6e70afd253c555709807e2491
jacobo <gilmith@gmail.com>
2025-05-02 22:53:16 +0200

    tiradas salvacion y categoria refactor

--- src/app/pages/main/personaje/ficha/ficha.component.ts
+++ src/app/pages/main/personaje/ficha/ficha.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { TiradasComponent } from "./tiradas/tiradas.component";
 import { Personaje } from '../../../../models/personaje';
 import { RazaComponent } from "./raza/raza.component";
@@ -21,10 +21,12 @@ export class FichaComponent {
   public showPrevious : boolean = false;
   public personaje? : Personaje;
   public step = signal<number>(0);
+  private personajeService = inject(PersonajeService);
   constructor(public servicio : PersonajeService) {}
 
   public actualizaPersonaje($event: Personaje) {
     this.personaje = $event;
+    console.log('valor de la señal', this.step);
     this.step.update(valor => valor++);
   }
 
@@ -38,4 +40,7 @@ export class FichaComponent {
     }
 
 
+  public isMago() : boolean {
+    return this.personajeService.personaje.categoria.nombre === "Mago" || this.personajeService.personaje.categoria.nombre === "Clerigo";
+  }
 }
